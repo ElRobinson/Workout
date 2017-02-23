983fd47c15bc1e05cca4de57695e88ffc525f5d3
ElRobinson <luisfelipe.robinson@gmail.com>
2017-02-23 16:30:59 -0300

    First Commit

---
+++ app/src/main/java/com/robinson/luis/sevenminutesworkout/TreinosAdapter.java
@@ -0,0 +1,72 @@
+package com.robinson.luis.sevenminutesworkout;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.robinson.luis.sevenminutesworkout.R.id.textView;
+
+/**
+ * Created by Luis on 21/02/2017.
+ */
+
+public class TreinosAdapter extends ArrayAdapter {
+
+    List list = new ArrayList();
+
+    public TreinosAdapter(Context context,int resource){
+        super(context,resource);
+    }
+
+    public void add(Object object){
+        super.add(object);
+        list.add(object);
+    }
+
+    public int getCount(){
+        return this.list.size();
+    }
+
+    public Object getItem(int position){
+        return this.list.get(position);
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent){
+        View row;
+
+        row = convertView;
+        DataHandler handler;
+
+        if(convertView == null){
+            LayoutInflater inflater = (LayoutInflater) this.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            row = inflater.inflate(R.layout.celula_treino,parent,false);
+            handler = new DataHandler();
+            handler.icone = (ImageView)row.findViewById(R.id.imageView);
+            handler.treino = (TextView)row.findViewById(R.id.txtIdTreino);
+            handler.inicial = (TextView)row.findViewById(R.id.txtNomeTreino);
+            row.setTag(handler);
+
+
+        } else {
+            handler = (DataHandler)row.getTag();
+        }
+
+        TreinosDataProvider dataProvider;
+        dataProvider = (TreinosDataProvider) this.getItem(position);
+        //handler.icone.setImageResource(dataProvider);
+        return row;
+    }
+
+    static class DataHandler{
+        ImageView icone;
+        TextView treino;
+        TextView inicial;
+    }
+}
---
+++ app/src/main/java/com/robinson/luis/sevenminutesworkout/TreinosDataProvider.java
@@ -0,0 +1,9 @@
+package com.robinson.luis.sevenminutesworkout;
+
+/**
+ * Created by Luis on 21/02/2017.
+ */
+
+public class TreinosDataProvider {
+
+}
